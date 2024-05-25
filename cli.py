c71de88c97b57826354bb5f19696691a07654418
jackyrx <jacky.rx@icloud.com>
2024-05-25 15:37:54 +0800

    add args --verbose

--- cli.py
+++ cli.py
@@ -8,11 +8,12 @@ def main():
     parser.add_argument('-s','--source', help='source file', required=True)
     parser.add_argument('-d', '--dest', help='destination folder', required=True)
     parser.add_argument('-t','--Thres', help='Threshold of the image difference', default=0.3)
+    parser.add_argument('-v', '--verbose', help='print verbose', action='store_true')
 
     args = parser.parse_args()
 
 
-    keyframeDetection(args.source, args.dest, float(args.Thres))
+    keyframeDetection(args.source, args.dest, float(args.Thres), args.verbose)
 
 if __name__ == '__main__':
     main()

c5db2a68804845b94fbba7edf1cc8760d5cd71f3
jackyrx <jacky.rx@icloud.com>
2024-05-25 15:39:16 +0800

    refractor + modify .csv export

--- KeyFrameDetector/key_frame_detector.py
+++ KeyFrameDetector/key_frame_detector.py
@@ -7,18 +7,18 @@ import peakutils
 from KeyFrameDetector.utils import convert_frame_to_grayscale, prepare_dirs, plot_metrics
 
 def keyframeDetection(source, dest, Thres, plotMetrics=False, verbose=False):
-    
-    keyframePath = dest+'/keyFrames'
-    imageGridsPath = dest+'/imageGrids'
-    csvPath = dest+'/csvFile'
+    keyframePath = dest + '/keyFrames'
+    imageGridsPath = dest + '/imageGrids'
+    csvPath = dest + '/csvFile'
     path2file = csvPath + '/output.csv'
     prepare_dirs(keyframePath, imageGridsPath, csvPath)
 
     cap = cv2.VideoCapture(source)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-  
-    if (cap.isOpened()== False):
+
+    if not cap.isOpened():
         print("Error opening video file")
+        return
 
     lstfrm = []
     lstdiffMag = []
@@ -27,12 +27,14 @@ def keyframeDetection(source, dest, Thres, plotMetrics=False, verbose=False):
     full_color = []
     lastFrame = None
     Start_time = time.process_time()
-    
+
     # Read until video is completed
     for i in range(length):
         ret, frame = cap.read()
         grayframe, blur_gray = convert_frame_to_grayscale(frame)
 
+        if blur_gray is None:
+            break
         frame_number = cap.get(cv2.CAP_PROP_POS_FRAMES) - 1
         lstfrm.append(frame_number)
         images.append(grayframe)
@@ -44,29 +46,38 @@ def keyframeDetection(source, dest, Thres, plotMetrics=False, verbose=False):
         diffMag = cv2.countNonZero(diff)
         lstdiffMag.append(diffMag)
         stop_time = time.process_time()
-        time_Span = stop_time-Start_time
+        time_Span = stop_time - Start_time
         timeSpans.append(time_Span)
         lastFrame = blur_gray
 
     cap.release()
     y = np.array(lstdiffMag)
     base = peakutils.baseline(y, 2)
-    indices = peakutils.indexes(y-base, Thres, min_dist=1)
-    
-    ##plot to monitor the selected keyframe
-    if (plotMetrics):
-        plot_metrics(indices, lstfrm, lstdiffMag)
+    indices = peakutils.indexes(y - base, Thres, min_dist=1)
+
+    # Plot to monitor the selected keyframe
+    if plotMetrics:
+        plot_metrics(indices, lstfrm, lstdiffMag, y)
+
+    # Initialize the CSV file with column names
+    csv_columns = ['Keyframe', 'Frame Number', 'Timestamp (sec)', 'Difference Magnitude']
+    with open(path2file, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(csv_columns)
 
     cnt = 1
     for x in indices:
-        cv2.imwrite(os.path.join(keyframePath , 'keyframe'+ str(cnt) +'.jpg'), full_color[x])
-        cnt +=1
+        cv2.imwrite(os.path.join(keyframePath, 'keyframe' + str(cnt) + '.jpg'), full_color[x])
         log_message = 'keyframe ' + str(cnt) + ' happened at ' + str(timeSpans[x]) + ' sec.'
-        if(verbose):
+        if verbose:
             print(log_message)
-        with open(path2file, 'w') as csvFile:
+
+        # Write the keyframe data to the CSV file
+        keyframe_data = [cnt, lstfrm[x], timeSpans[x], lstdiffMag[x]]
+        with open(path2file, 'a', newline='') as csvFile:
             writer = csv.writer(csvFile)
-            writer.writerows(log_message)
-            csvFile.close()
+            writer.writerow(keyframe_data)
+        cnt += 1
 
     cv2.destroyAllWindows()
+    
